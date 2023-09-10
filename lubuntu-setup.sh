5894a1bd204a825ceb98ed296b9eb1331321a8da
Pui Kit Li <59538991+PKLJack@users.noreply.github.com>
2023-09-10 12:21:48 +0100

    Disable mkfs.ext4 on logical volumes

--- lubuntu-setup.sh
+++ lubuntu-setup.sh
@@ -72,7 +72,7 @@ echo '# LVM Stuff'
 echo '#########################'
 
 
-# Physical Volumnes
+# Physical Volumes
 pvcreate /dev/sdb2 /dev/sda1
 
 #  Volume Groups
@@ -84,10 +84,10 @@ lvcreate --size 12GiB --name lv_root vg_root /dev/sdb2
 lvcreate --size 8GiB --name lv_home vg_root /dev/sda1
 lvcreate --size 10GiB --name lv_var vg_root /dev/sda1
 
-# Make file systems on logical volumns
-mkfs.ext4 -q /dev/mapper/vg_root-lv_home
-mkfs.ext4 -q /dev/mapper/vg_root-lv_root
-mkfs.ext4 -q /dev/mapper/vg_root-lv_var
+# # Make file systems on logical volumes
+# mkfs.ext4 -q /dev/mapper/vg_root-lv_home
+# mkfs.ext4 -q /dev/mapper/vg_root-lv_root
+# mkfs.ext4 -q /dev/mapper/vg_root-lv_var
 
 
 echo '#########################'
