6e0f6fda43dca65aa549a34fcdf20756304c5dd5
Pui Kit Li <59538991+PKLJack@users.noreply.github.com>
2023-09-10 11:17:32 +0100

    Update parted commands with --script option

--- lubuntu-setup.sh
+++ lubuntu-setup.sh
@@ -9,23 +9,23 @@ wipefs --all /dev/sda
 echo '#########################'
 echo '# partition table'
 echo '#########################'
-parted /dev/sda -- mktable gpt
-parted /dev/sdb -- mktable gpt
+parted --script /dev/sda -- mktable gpt
+parted --script /dev/sdb -- mktable gpt
 
-# parted /dev/sda -- p ; parted /dev/sdb -- p
+# parted --script /dev/sda -- p ; parted --script /dev/sdb -- p
 
 
 echo '#########################'
 echo '# Make partitions'
 echo '#########################'
 # HDD
-parted /dev/sda -- mkpart 'hdd' ext4 0% 100%
+parted --script /dev/sda -- mkpart 'hdd' ext4 0% 100%
 
 # SSD
-parted /dev/sdb -- mkpart 'boot' fat32 1MiB 301MiB
-parted /dev/sdb -- mkpart 'root' ext4 301MiB 100%
+parted --script /dev/sdb -- mkpart 'boot' fat32 1MiB 301MiB
+parted --script /dev/sdb -- mkpart 'root' ext4 301MiB 100%
 
-# parted /dev/sda -- p ; parted /dev/sdb -- p
+# parted --script /dev/sda -- p ; parted --script /dev/sdb -- p
 
 
 echo '#########################'
@@ -33,16 +33,16 @@ echo '# Set flags'
 echo '#########################'
 
 # sda1
-parted /dev/sda -- set 1 lvm on
+parted --script /dev/sda -- set 1 lvm on
 
 # sdb1
-parted /dev/sdb -- set 1 boot on
-parted /dev/sdb -- set 1 esp on
+parted --script /dev/sdb -- set 1 boot on
+parted --script /dev/sdb -- set 1 esp on
 
 # sdb2
-parted /dev/sdb -- set 2 lvm on
+parted --script /dev/sdb -- set 2 lvm on
 
-# parted /dev/sda -- p ; parted /dev/sdb -- p
+# parted --script /dev/sda -- p ; parted --script /dev/sdb -- p
 
 
 echo '#########################'
@@ -53,7 +53,7 @@ echo 'Keep /dev/sda1 without file system.'
 mkfs.vfat -F 32 /dev/sdb1
 # mkfs.ext4 /dev/sdb2
 
-# parted /dev/sdb -- p
+# parted --script /dev/sdb -- p
 
 
 echo '#########################'
